04ee4f9f7dc78f8193ef7a87ccf07459e8dd21ef
viteklu15 <46672002+viteklu15@users.noreply.github.com>
2026-01-31 11:04:54 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,11 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('pwa-cache').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
       ]);
     })
   );
